055b4807d1f60cf8926d8d92664d3ebe2715bbfb
toniiplaycode <toan13022017@gmail.com>
2023-09-30 20:30:45 +0700

    update

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from '../constants';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;

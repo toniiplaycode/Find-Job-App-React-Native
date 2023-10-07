dcb8e92811296e510fed11f9fd31c4f27ec8a400
toniiplaycode <toan13022017@gmail.com>
2023-10-07 09:32:46 +0700

    update

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,12 +1,16 @@
-import { View } from "react-native"
+import { Text, View, RefreshControl, ScrollView, ActivityIndicator } from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
 
 const JobDetails = () => {
     return (
-        <View>
+        <ScrollView>
             <Text>
                 job details
             </Text>
-        </View>
+            
+            <RefreshControl refreshing={true} onRefresh={onRefresh}/>
+        </ScrollView>
     )
 }
 

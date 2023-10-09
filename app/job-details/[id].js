cc948785435114300e751e771c433e3cfc7b3cac
toniiplaycode <toan13022017@gmail.com>
2023-10-09 20:26:43 +0700

    update

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,15 +1,26 @@
 import { Text, View, RefreshControl, ScrollView, ActivityIndicator } from "react-native";
 import { Stack, useRouter, useSearchParams } from "expo-router";
 import { useCallback, useState } from "react";
+import useFetch from "../../hook/useFetch";
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../ ../components';
+import { COLORS, icons, SIZES } from '../../constants';
 
 const JobDetails = () => {
+    const params = useSearchParams();    
+    const router = useRouter();
+    
+    const { data, isLoading, error, reFetch } = useFetch(
+        'job-details',
+        {
+            job_id: params.id
+        }
+    )
+
     return (
         <ScrollView>
             <Text>
                 job details
-            </Text>
-            
-            <RefreshControl refreshing={true} onRefresh={onRefresh}/>
+            </Text>            
         </ScrollView>
     )
 }
